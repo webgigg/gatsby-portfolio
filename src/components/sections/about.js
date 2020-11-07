cc4e76e6041d908a0a4fba55c639e657506afe18
webgigg <visiondev18@gmail.com>
2020-11-08 01:46:44 +1000

    minor change

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -141,7 +141,7 @@ const About = () => {
       <div className="inner">
         <StyledText>
           <div>
-            <p>Hello! I'm Brittany, a software engineer based in Boston, MA.</p>
+            <p>Hello! I'm Dev, a software engineer based in Fontana, CA.</p>
 
             <p>
               I enjoy creating things that live on the internet, whether that be websites,
